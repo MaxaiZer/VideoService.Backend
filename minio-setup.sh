9561d4ed99fff1f862d494fdb7cac56a22af5ee0
MaxaiZer <51049295+MaxaiZer@users.noreply.github.com>
2024-09-27 17:52:28 +0700

    fix: handle case when bucket is already created

--- minio-setup.sh
+++ minio-setup.sh
@@ -7,11 +7,11 @@ until /usr/bin/mc alias set myminio http://127.0.0.1:9000 "$MINIO_ROOT_USER" "$M
   sleep 5
 done
 
-if ! /usr/bin/mc mb myminio/"$BUCKET_NAME"; then
-  echo "Error: Unable to create bucket '$BUCKET_NAME'."
-  exit 1
+if /usr/bin/mc mb myminio/"$BUCKET_NAME" --ignore-existing; then
+    echo "Bucket '$BUCKET_NAME' created successfully."
+  else
+    echo "Error: Unable to create bucket '$BUCKET_NAME'."
+    exit 1
 fi
-
-echo "Bucket '$BUCKET_NAME' created successfully."
 
 wait
