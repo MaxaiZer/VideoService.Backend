511c09c823dbc8908ef05fbecce51cda5ce3b3e1
MaxaiZer <51049295+MaxaiZer@users.noreply.github.com>
2024-09-29 02:00:28 +0700

    build: improve logging in minio setup script

--- minio-setup.sh
+++ minio-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 minio server --console-address ":9001" /data &
+echo "MinIO server was started"
 
 until /usr/bin/mc alias set myminio http://127.0.0.1:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"; do
-  echo "Waiting for MinIO server..."
+  echo "Waiting for MinIO server to be ready..."
   sleep 5
 done
 
