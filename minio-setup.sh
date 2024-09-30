46f046cb2bb8a08929f7b1c024bec4953a972c9f
MaxaiZer <51049295+MaxaiZer@users.noreply.github.com>
2024-10-01 03:11:30 +0700

    fix: add health check to wait for MinIO readiness

--- minio-setup.sh
+++ minio-setup.sh
@@ -3,10 +3,12 @@
 minio server --console-address ":9001" /data &
 echo "MinIO server was started"
 
-until /usr/bin/mc alias set myminio http://127.0.0.1:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"; do
-  echo "Waiting for MinIO server to be ready..."
-  sleep 5
+until curl -s http://127.0.0.1:9000/minio/health/live; do
+    echo "Waiting for MinIO server to be ready..."
+    sleep 5
 done
+
+/usr/bin/mc alias set myminio http://127.0.0.1:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD";
 
 if /usr/bin/mc mb myminio/"$BUCKET_NAME" --ignore-existing; then
     echo "Bucket '$BUCKET_NAME' created successfully."
